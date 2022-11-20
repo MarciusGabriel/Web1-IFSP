5fb0cdf2203e30e16f86ed38cde282ab6c44d830
Marcius Gabriel <105213014+MarciusGabriel@users.noreply.github.com>
2022-11-20 19:57:14 -0300

    add

---
+++ jquery/lorem/praticando1.js
@@ -0,0 +1,50 @@
+/*function alerta() {
+    let nome = prompt("Qual é o seu nome?")
+    let texto = "  <h2> Seja bem vindo " + nome + "</h2>"
+    document.getElementById("nome").innerHTML = texto
+    return 0
+}
+
+function e() {
+    let lorem = "<p id= 'paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias qui a illo iure fuga omnis praesentium vero, dolore quos ea mollitia nam nemo eum, soluta facere dolorum sequi blanditiis in.</p>"
+    document.getElementById("texto2").innerHTML += lorem
+}
+
+function f() {
+    let nome = document.getElementById("texto2").remove()
+    let contador
+    contador++
+    if (contador != 1) {
+        let kkkk = "<p id='texto2' ></p>"
+        document.getElementById("quatro").innerHTML = kkkk
+    }
+
+} 
+
+*/
+
+$(function alerta (){
+    let nome = prompt("Qual é o seu nome?")
+    let texto = "  <h2> Seja bem vindo " + nome + "</h2>"
+    document.getElementById("nome").innerHTML=texto
+    return 0
+}) 
+
+
+
+$("#adicionar").click(function adicionar(){
+    $("#texto2").append("<p id= 'paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam totam fuga neque voluptas veritatis omnis et tempore consequuntur hic fugit aliquam, tenetur quos accusamus laudantium nihil, enim voluptate ex obcaecati quae earum eius itaque. Laudantium, odio!</p>");
+});
+
+
+$("#remover").click(function remover() {     $("p").empty() })
+
+
+
+
+
+
+
+
+
+
